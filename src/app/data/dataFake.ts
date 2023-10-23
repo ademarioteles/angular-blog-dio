c6677ffe6cb573927dc13a05a6ac537142ef5cfe
Ademario <ademarioteles@hotmail.com>
2023-10-22 22:43:50 -0300

    commit final

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,20 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photo":'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/3/35/IronMan-EndgameProfile.jpg/revision/latest/scale-to-width-down/1200?cb=20190423175213',
+    "title":'',
+    "description":'ola mundo, obrigado por tudo. 1'
+  },
+  {
+    "id":"2",
+    "photo":'https://media.gettyimages.com/id/1387199069/photo/los-angeles-california-cosplaying-fan-dressed-as-tony-stark-attends-the-premiere-of-marvel.jpg?s=612x612&w=gi&k=20&c=SHICst5RiGYHM_ejT5tZIOpvmk7kHclr661f6qpJVwk=',
+    "title":'',
+    "description":'ola mundo, obrigado por tudo. 2 '
+  },
+  {
+    "id":"3",
+    "photo":'https://wellgroomedgentleman.com/wp-content/uploads/2023/10/Tony_Stark_Beard_with_Quiff_Hairstyle.width-800.jpg',
+    "title":'',
+    "description":'ola mundo, obrigado por tudo.'
+  }
+]
